2c514aa87e75d7bb421314099f65b39e82ebed1e
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-05-04 23:28:21 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_05_04_232302) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
